99d4d4527105b41278b4856ef87cbfc2abbb53e0
PUSHkin42342443 <alexandderdudarev@gmail.com>
2021-10-23 18:28:43 +0500

    Выполнил задачу 4, но не могу правильно вывести на экран значаение(

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 13;
+            double a = 2 + Math.Sqrt(Math.Tan(3));
+            double b = 7 + Math.Sqrt(Math.Tan(8));
+            double c = 1 + Math.Sqrt(Math.Tan(4));
+            double d = a * UmnozenieOne(x);
+            double e = c * UmnozenieOne(x);
+            double f = d * UmnozenieOne(x);
+            double g = (d + e + f) / UmnozenieOne(x);
+
+            Console.WriteLine(@" {d:##,##:##} ");
+            Console.ReadKey();
+        }
+
+        static double UmnozenieOne(double x)
+        {
+
+
+            return x * 17 * 113;
+
+
+        }
+
+    }
+}
